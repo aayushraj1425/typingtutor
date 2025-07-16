76e867383e4949b0f61f159a416424d8ba063697
Aayush Raj Sah <aayushraj1425@gmail.com>
2025-07-16 13:51:53 -0500

    Updated layout and added some functionality.

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,113 @@
+import words from "./words.js"
+
+let time = {"time1": 30, "time2" : 60, "time3": 120};
+let level = {"level1": "standard", "level2": "professional"};
+
+const time1 = document.querySelector("#time1")
+const time2 = document.querySelector("#time2")
+const time3 = document.querySelector("#time3")
+const level1 = document.querySelector("#level1")
+const level2 = document.querySelector("#level2")
+const text = document.querySelector("#test-text")
+const mins = document.querySelector('#minutes')
+const secs = document.querySelector('#seconds')
+
+let test_time, test_level, test_words; 
+
+time1.onclick = function () {
+    setDisplayTime("time1");
+    typingTest();
+}
+time2.onclick = function () {
+    setDisplayTime("time2");
+}
+time3.onclick = function () {
+    setDisplayTime("time3");
+}
+level1.onclick = function () {
+    setLevel("level1");
+}
+level2.onclick = function () {
+    setLevel("level2");
+}
+
+function setDisplayTime(time_type) {
+    test_time = time[time_type];
+    mins.textContent = Math.floor(test_time / 60);
+    secs.textContent = `${test_time % 60 < 10 ? '0' : ''}${test_time % 60}`;
+}
+
+function setLevel(level_type) {
+    test_level = level[level_type];
+    test_words = words[level_type];
+}
+
+setDisplayTime("time1");
+setLevel("level1");
+
+let typing = false;
+let current_key;
+
+const user_typing = new Promise((resolve) => {
+    document.addEventListener('keydown', (event) => {
+        console.log("User typed:" + event.key)
+        current_key = event.key;
+        typing = true;
+        resolve();
+    });
+}
+)
+
+// Countdown Timer
+function startTimer(durationInSeconds) {
+  var remainingTime = durationInSeconds;
+  const timerInterval = setInterval(() => {
+    const minutes = Math.floor(remainingTime / 60);
+    const seconds = remainingTime % 60;
+
+    // console.log(`${minutes}:${seconds < 10 ? '0' : ''}${seconds}`);
+    mins.textContent = `${minutes}`;
+    secs.textContent = `${seconds < 10 ? '0' : ''}${seconds}`;
+
+    if (remainingTime <= 0) {
+      clearInterval(timerInterval);
+      console.log("Time's up!");
+    }
+
+    remainingTime--;
+  }, 1000); 
+}
+
+
+async function typingTest () {
+    // const timer = new Promise((resolve) => {
+    //     startTimer(test_time);
+    //     resolve("Time done");
+    // });
+    let correct_words_typed = [];
+    let words_typed = [];
+    let time = test_time;
+    let current_word_index = 0;
+    let current_letter_index = 0;
+    text.textContent = test_words(0, 30);
+
+    await user_typing;
+    startTimer(test_time);
+    while (typing == true && remainingTime > 0) {
+        if (current_key === test_words[current_word_index][current_letter_index]) {
+            console.log("Yes");
+            await user_typing;
+            if (current_key != "Backspace") {
+                current_letter_index++;
+            }
+        }
+    }
+    
+    // await timer;
+    // timer.then((result) => {
+    //     console.log("Time done:", result);
+    // })
+};
+
+typingTest();
 
---
+++ static/js/words.js
@@ -0,0 +1,31 @@
+const standard_text = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.";
+const professional_text = "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
+
+const standard_words = standard_text.replace(/[^a-zA-Z\s]/g, "").split(' ');
+const professional_words = professional_text.split(' ');
+
+function shuffle(words_array) {
+  let array = JSON.parse(JSON.stringify(words_array));
+
+  let currentIndex = array.length;
+
+  while (currentIndex != 0) {
+
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+  return array;
+}
+
+let shuffled_standard_words = shuffle(standard_words);
+let professional_standard_words = shuffle(professional_words);
+
+let words = {
+    "level1": shuffled_standard_words, 
+    "level2": professional_standard_words,
+}
+
+export default words
